3f339e6fc87ce47b893e83994d7f0ff5933a08e5
SimonThormeyer <simon.thormeyer@wire.com>
2026-01-25 14:04:05 +0100

    feat: basic flow of creating a military unit
    The state loop receives command of creating a unit, requests the payment
    service to issue the payment and then creates the unit.

--- src/simulation.rs
+++ src/simulation.rs
@@ -4,15 +4,10 @@ use std::time::Duration;
 
 use tokio::sync::mpsc;
 
-use crate::{geometry::Point, military::BaseId, money::Costs, state::Command};
+use crate::{geometry::Point, military::BaseId, state::Command};
 
 /// Run the simulation, allow it to send [Command]s via a channel.
 pub(super) async fn simulation(tx: mpsc::Sender<Command>) {
-    // This is most likely not supposed to live here.
-    let payment_service = Costs {
-        military_unit: Default::default(),
-    };
-
     let position = Point::new(3.0, 4.0);
     loop {
         // Just create a military unit every second.
@@ -20,13 +15,6 @@ pub(super) async fn simulation(tx: mpsc::Sender<Command>) {
         // TODO: in the future, we'd get this id from the existing base that produces the unit. Taking a shortcut here
         // for now.
         let base_id = BaseId(0);
-        let payment = payment_service.pay_for_military_unit();
-        let _ = tx
-            .send(Command::CreateUnit {
-                payment,
-                base_id,
-                position,
-            })
-            .await;
+        let _ = tx.send(Command::CreateUnit { base_id, position }).await;
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -8,8 +8,8 @@ use typed_builder::TypedBuilder;
 use crate::{
     config::Config,
     geometry::Point,
-    military::{BaseId, MilitaryUnit, MilitaryUnitCost},
-    money::Payment,
+    military::{BaseId, MilitaryUnit},
+    payment_service::PaymentService,
 };
 
 #[derive(TypedBuilder)]
@@ -26,11 +26,7 @@ type ReadCommand<T> = Sender<OwnedRwLockReadGuard<T>>;
 /// Used to query or mutate the state of the [state_loop].
 #[derive(Debug)]
 pub(super) enum Command {
-    CreateUnit {
-        payment: Payment<MilitaryUnitCost>,
-        base_id: BaseId,
-        position: Point,
-    },
+    CreateUnit { base_id: BaseId, position: Point },
     GetUnits(ReadCommand<Vec<MilitaryUnit>>),
 }
 
@@ -43,19 +39,18 @@ impl State {
         // Load units from database
         let units = Arc::new(RwLock::new(vec![]));
 
+        let payment_service = PaymentService {
+            military_unit: self.config.costs().unit(),
+        };
+
         while let Some(cmd) = self.receiver.recv().await {
             match cmd {
                 Command::GetUnits(resp) => {
                     let units_guard = units.clone().read_owned().await;
                     let _ = resp.send(units_guard);
                 }
-                Command::CreateUnit {
-                    payment,
-                    base_id,
-                    position,
-                } => {
-                    let cost = payment.cost();
-                    log::debug!("creating unit... paid {cost:?}");
+                Command::CreateUnit { base_id, position } => {
+                    let payment = payment_service.pay_for_military_unit();
                     let unit = MilitaryUnit::new(payment, base_id, position);
                     let mut units_guard = units.write().await;
                     units_guard.push(unit);
