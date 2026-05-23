ca9f6843c1dec02c13810c0126edc9fdbdc79c56
SimonThormeyer <simon.thormeyer@wire.com>
2026-05-23 12:09:24 +0200

    chore: `MilitaryUnit` also has an id

--- src/domain/unit.rs
+++ src/domain/unit.rs
@@ -1,3 +1,4 @@
+use mongodb::bson::Uuid;
 use serde::Serialize;
 
 use crate::{
@@ -6,9 +7,21 @@ use crate::{
     services::payment_service::{Payment, SinglePayer},
 };
 
+/// Identifies a [MilitaryUnit].
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, derive_more::From, derive_more::Into)]
+pub(crate) struct UnitId(String);
+
+impl UnitId {
+    fn new() -> Self {
+        let id = Uuid::new();
+        Self(id.to_string())
+    }
+}
+
 /// Associated with a [MilitaryBase] and a [Bloc]. The [Bloc] association is implicit.
 #[derive(Debug, Clone, Serialize)]
 pub(crate) struct MilitaryUnit {
+    id: UnitId,
     base_id: BaseId,
     position: Point,
 }
@@ -17,11 +30,23 @@ crate::impl_positioned!(MilitaryUnit => position);
 
 impl MilitaryUnit {
     pub(crate) fn new(_payment: Payment<Self, SinglePayer>, base_id: BaseId, position: Point) -> Self {
-        Self { base_id, position }
+        Self {
+            id: UnitId::new(),
+            base_id,
+            position,
+        }
     }
 
-    pub(crate) fn from_persisted(base_id: BaseId, position: Point) -> Self {
-        Self { base_id, position }
+    pub(crate) fn id(&self) -> &UnitId {
+        &self.id
+    }
+
+    pub(crate) fn from_persisted(id: String, base_id: BaseId, position: Point) -> Self {
+        Self {
+            id: id.into(),
+            base_id,
+            position,
+        }
     }
 
     pub(crate) fn base_id(&self) -> BaseId {
--- src/persistence/units.rs
+++ src/persistence/units.rs
@@ -1,18 +1,16 @@
 use anyhow::Result;
-use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
+use super::parse_id;
 use crate::{
     domain::{BaseId, MilitaryUnit},
     geometry::{Point, Positioned},
 };
 
-use super::parse_id;
-
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub(super) struct PersistedUnit {
     #[serde(rename = "_id")]
-    id: ObjectId,
+    id: String,
     base_id: String,
     position: Point,
 }
@@ -20,7 +18,7 @@ pub(super) struct PersistedUnit {
 impl PersistedUnit {
     pub(super) fn from_unit(unit: &MilitaryUnit) -> Self {
         Self {
-            id: ObjectId::new(),
+            id: unit.id().clone().into(),
             base_id: unit.base_id().0.to_string(),
             position: unit.position(),
         }
@@ -28,6 +26,6 @@ impl PersistedUnit {
 
     pub(super) fn into_unit(self) -> Result<MilitaryUnit> {
         let base_id = parse_id::<BaseId>(&self.base_id, "unit base")?;
-        Ok(MilitaryUnit::from_persisted(base_id, self.position))
+        Ok(MilitaryUnit::from_persisted(self.id, base_id, self.position))
     }
 }
